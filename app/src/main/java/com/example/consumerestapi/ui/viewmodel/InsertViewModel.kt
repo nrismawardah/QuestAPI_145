69bc348f8b594251603ebd9b775a37f728c9f70e
nrismawardah <nrismawardah@gmail.com>
2024-12-29 03:50:51 +0700

    Menambahkan class InsertViewModel

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -1,6 +1,33 @@
 package com.example.consumerestapi.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumerestapi.model.Mahasiswa
+import com.example.consumerestapi.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val mhs: MahasiswaRepository): ViewModel(){
+
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent:InsertUiEvent){
+        uiState=InsertUiState(insertUiEvent=insertUiEvent)
+    }
+
+    suspend fun insertMhs(){
+        viewModelScope.launch{
+            try{
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch(e:Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
     nim = nim,
