30e062313324d8252776fa57383593528078cede
nrismawardah <nrismawardah@gmail.com>
2024-12-29 03:46:54 +0700

    Menambahkan function untuk konversi antara Mahasiswa, InsertUiEvent, dan InsertUiState

--- app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/viewmodel/InsertViewModel.kt
@@ -1,5 +1,29 @@
 package com.example.consumerestapi.ui.viewmodel
 
+import com.example.consumerestapi.model.Mahasiswa
+
+fun InsertUiEvent.toMhs(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
 data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
