cfff2abbb09a8b2e0fa6f2504ec4aeb5cfe221ea
nrismawardah <nrismawardah@gmail.com>
2024-12-29 04:30:01 +0700

    Membuat object PenyediaViewModel dan function CreationExtras.mahasiswaApp

---
+++ app/src/main/java/com/example/consumerestapi/PenyediaViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.consumerestapi
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.consumerestapi.ui.viewmodel.HomeViewModel
+import com.example.consumerestapi.ui.viewmodel.InsertViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(mahasiswaApp().container.mahasiswaRepository) }
+        initializer { InsertViewModel(mahasiswaApp().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.mahasiswaApp(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
