8ab1187f5d5a732826fea3c994277c9d4fd80e37
nrismawardah <nrismawardah@gmail.com>
2024-12-29 23:34:16 +0700

    Memperbarui tampilan DetailView

--- app/src/main/java/com/example/consumerestapi/ui/view/DetailView.kt
+++ app/src/main/java/com/example/consumerestapi/ui/view/DetailView.kt
@@ -1,10 +1,42 @@
 package com.example.consumerestapi.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.consumerestapi.model.Mahasiswa
 import com.example.consumerestapi.ui.PenyediaViewModel
+import com.example.consumerestapi.ui.customwidget.TopAppBar
 import com.example.consumerestapi.ui.navigation.DestinasiNavigasi
+import com.example.consumerestapi.ui.viewmodel.DetailMhsUiState
 import com.example.consumerestapi.ui.viewmodel.DetailViewModel
 
 object DestinasiDetail : DestinasiNavigasi {
@@ -14,6 +46,7 @@ object DestinasiDetail : DestinasiNavigasi {
     val routeWithArgs = "$route/{$nim}"
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailView(
     modifier: Modifier = Modifier,
@@ -21,5 +54,134 @@ fun DetailView(
     onEditClick: (String) -> Unit,
     detailViewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                title = DestinasiDetail.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    val nim = (detailViewModel.detailMhsUiState as? DetailMhsUiState.Success)?.mahasiswa?.nim
+                    if (nim != null) onEditClick(nim)
+                },
+                shape = MaterialTheme.shapes.medium
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Mahasiswa",
+                )
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding).offset(y = (-70).dp)
+        ) {
+            DetailStatus(
+                mhsUiState = detailViewModel.detailMhsUiState,
+                retryAction = { detailViewModel.getMhsbyNim() },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.Center)
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailStatus(
+    mhsUiState: DetailMhsUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    when (mhsUiState) {
+        is DetailMhsUiState.Success -> {
+            DetailCard(
+                mahasiswa = mhsUiState.mahasiswa,
+                modifier = modifier.padding(16.dp)
+            )
+        }
+
+        is DetailMhsUiState.Loading -> {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
+
+        is DetailMhsUiState.Error -> {
+            Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "Terjadi kesalahan.")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(onClick = retryAction) {
+                        Text(text = "Coba Lagi")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DetailCard(
+    mahasiswa: Mahasiswa,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            //horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            ComponentDetailMhs(judul = "NIM", isinya = mahasiswa.nim)
+            Spacer(modifier = Modifier.height(8.dp))
+            ComponentDetailMhs(judul = "Nama", isinya = mahasiswa.nama)
+            Spacer(modifier = Modifier.height(8.dp))
+            ComponentDetailMhs(judul = "Alamat", isinya = mahasiswa.alamat)
+            Spacer(modifier = Modifier.height(8.dp))
+            ComponentDetailMhs(judul = "Jenis Kelamin", isinya = mahasiswa.jenisKelamin)
+            Spacer(modifier = Modifier.height(8.dp))
+            ComponentDetailMhs(judul = "Kelas", isinya = mahasiswa.kelas)
+            Spacer(modifier = Modifier.height(8.dp))
+            ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "$judul:",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
