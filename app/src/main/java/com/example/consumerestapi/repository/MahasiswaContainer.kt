c7fa911bb881c6cac59fd383966eabedf6a0856a
nrismawardah <nrismawardah@gmail.com>
2024-12-29 21:14:25 +0700

    Mengganti alamat IP  baseUrl pada MahasiswaContainer

--- app/src/main/java/com/example/consumerestapi/repository/MahasiswaContainer.kt
+++ app/src/main/java/com/example/consumerestapi/repository/MahasiswaContainer.kt
@@ -11,7 +11,7 @@ interface AppContainer {
 }
 
 class MahasiswaContainer : AppContainer {
-    private val baseUrl = "http://10.0.2.2:8000/umyTI/"
+    private val baseUrl = "http://192.168.18.133/umyTI/"
     private val json = Json { ignoreUnknownKeys = true }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
