ac1ba1e8afe7c90f8e122faa2caa578a5fce63f3
nrismawardah <nrismawardah@gmail.com>
2024-12-29 16:36:36 +0700

    Memanggil MahasiswaApp di MainActivity dan menambahkan code pada MahasiswaApp

--- app/src/main/java/com/example/consumerestapi/MahasiswaApp.kt
+++ app/src/main/java/com/example/consumerestapi/MahasiswaApp.kt
@@ -13,7 +13,9 @@ import com.example.consumerestapi.ui.navigation.PengelolaHalaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MahasiswaApp(){
+fun MahasiswaApp(
+    modifier: Modifier = Modifier
+){
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         modifier = Modifier
--- app/src/main/java/com/example/consumerestapi/MainActivity.kt
+++ app/src/main/java/com/example/consumerestapi/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ConsumeRestAPITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MahasiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
